91eb23a3736c3faa329e45c9d2f65e1f36779c4f
yuyedaidao <wyqpadding@gmail.com>
2015-10-14 23:13:16 +0800

    Initial Commit

---
+++ YQSweepCardView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQSweepCardView
+//
+//  Created by 王叶庆 on 15/10/14.
+//  Copyright © 2015年 王叶庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
